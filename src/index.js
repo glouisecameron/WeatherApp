d4fba13cb8b5d3185fa2cd42a81ce894777cf506
glouisecameron <g.louise.cameron@live.com>
2022-03-21 15:17:50 +0000

    Console logging get forecast function from API

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ days.forEach(function (day) {
   
 })
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast")
  let days = ["Thu", "Fri", "Sat", "Sun"];
 
@@ -47,6 +48,12 @@ function displayForecast() {
   
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "8fe09840c8f6529695672e7366eacd10";
+   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function changeCity(event) {
   event.preventDefault();
@@ -64,7 +71,7 @@ function searchCity(city) {
 }
 
 searchCity("Glasgow");
-displayForecast();
+
 
 let changeButton = document.querySelector("form");
 changeButton.addEventListener("submit", changeCity);
@@ -94,6 +101,7 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 
+  getForecast(response.data.coord);
   celciusTemperature = response.data.main.temp;
 
 }
