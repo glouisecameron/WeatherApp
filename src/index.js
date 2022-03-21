87c29b512077bbb71db0bbe82976fe913b8696dc
glouisecameron <g.louise.cameron@live.com>
2022-03-21 15:58:37 +0000

    Attempt to connect daily forecast to API

--- src/index.js
+++ src/index.js
@@ -19,25 +19,25 @@ days.forEach(function (day) {
 })
 
 function displayForecast(response) {
-  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast")
- let days = ["Thu", "Fri", "Sat", "Sun"];
-
-  let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
+let forecastHTML = `<div class="row">`;
+  days.forEach(function (forecastDay, index) {
+    if (index <5)
     forecastHTML =
       forecastHTML +
       `
-      <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+      <div class="col">
+      <div class "weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
-          width="42"
+          width="40"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max">${math.round(forecastDay.temp.max)}° </span>
+          <span class="weather-forecast-temperature-min">${math.round(forecastDay.temp.min)}°</span>
         </div>
       </div>
   `;
